1f3fe2aa3bc444d3ae5d2c7905f3bd307680ef02
iqbalcrat <iqbalhonnur@gmail.com>
2024-02-16 10:47:32 +0530

    second

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+    <img src="/img/logo.png" alt="Net Ninja Logo" >
+</header>
+
+
+<style>
+    header{
+        background: #f7f7f7;
+        padding: 20px;
+    }
+    h1{
+        margin: 0;
+        text-align: center;
+    }
+    img{
+        height: 50px; width:50px
+    }
+</style>
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    export let items
+    export let activeItem
+</script>
+
+
+<div class="tabs">
+
+    <ul>
+        {#each items as item}
+            <li on:click={() => dispatch('tabChange', item)}>
+                <div class:active={item === activeItem}>{item}</div>
+            </li>
+        {/each}
+    </ul>
+
+</div>
+
+
+<style>
+.tabs{
+    margin-bottom: 40px;
+}
+ul{
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+}
+.active{
+    color:red;
+    border-bottom: 2px solid red;
+    padding-bottom: 8px;
+}
+
+li{
+    margin: 0 16px;
+    font-size: 18px;
+    color: #555;
+    cursor: pointer;
+}
+
+</style>
